1a61fa064a94517101e3ac7b87deac7d1cc61efa
ChuSebastian <sebastian.chu@utec.edu.pe>
2025-06-14 11:23:19 -0500

    comentario.py editado para guadar en bucket s3 usando push

--- comentario.py
+++ comentario.py
@@ -14,12 +14,25 @@ def lambda_handler(event, context):
         'tenant_id': tenant_id,
         'uuid': uuidv1,
         'detalle': {
-          'texto': texto
+            'texto': texto
         }
     }
+
+    # Grabar en S3 (Ingesta Push)
+    s3 = boto3.client('s3')
+    file_key = f'{tenant_id}/{uuidv1}.json'
+    s3.put_object(
+        Bucket=bucket_ingesta,
+        Key=file_key,
+        Body=json.dumps(comentario),
+        ContentType='application.json'
+    )
+
+    # Grabar en DynamoDB
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+
     # Salida (json)
     print(comentario)
     return {
